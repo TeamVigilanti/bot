89ba418df709e811e89c645e62b9242b9d839159
Codeize <mrcodeize@gmail.com>
2021-10-17 16:21:03 +0100

    working on embed cmd for fad locally

--- commands/misc/embed.js
+++ commands/misc/embed.js
@@ -11,8 +11,18 @@ module.exports = {
     run (client, message, args) {
         try {
             const said = JSON.parse(args.join(' '))
-            const embed = new MessageEmbed(said)
 
+            /* EMBED ERR HANDLING */
+            if (said.title?.length > 256) throw new Error('Title must be less than or equal to 256 characters')
+            if (said.description?.length > 4096) throw new Error('Title must be less than or equal to 4096 characters')
+            for (field of said.fields) {
+                if (field?.name?.length > 256) throw new Error('Field names must be less than or equal to 256 characters')
+                if (field?.value?.length > 1024) throw new Error('Field values must be less than or equal to 1024 characters')
+            }
+            /* EMBED ERR HANDLING */
+
+            const embed = new MessageEmbed(said)
+            console.log('We stopped here')
             message.channel.send({ embeds: [embed] })
         } catch (err) {
             const errEmbed = ccEmbed('error', 'Invalid JSON!', 'Please use a valid json format\nEg: \n```json\n{"title": "hi"}\n```')
