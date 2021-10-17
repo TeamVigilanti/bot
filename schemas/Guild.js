2c1f58fef1a0e9d024b7ad0d2887fd3943f2bd59
Codeize <mrcodeize@gmail.com>
2021-10-17 20:30:10 +0100

    Update Guild.js

--- schemas/Guild.js
+++ schemas/Guild.js
@@ -3,6 +3,7 @@ const { prefix } = require('../config.json')
 
 const GuildSchema = new Schema({
     _id: { type: String },
+    premium: { type: Boolean, default: true }, // premium will be free for the first 75 servers and will be granted 'legacy server' perks
     config: {
         prefix: { type: String, default: prefix },
         mutedRole: String,
