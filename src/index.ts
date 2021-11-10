8c6f53ab4da01d28aca5b51a513fd4edc08506de
Fad F <90547391+FadTheChad@users.noreply.github.com>
2021-11-10 18:15:41 +0500

    renamed class in index file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import CrowdControlClient from "./Client/CrowdControlClient";
+import VigilantiClient from "./Client/VigilantiClient";
 import { config } from "dotenv";
 config();
 
-const client:CrowdControlClient = new CrowdControlClient({intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_VOICE_STATES"], partials: ["USER", "CHANNEL", "GUILD_MEMBER", "MESSAGE", "REACTION"]});
+const client:VigilantiClient = new VigilantiClient({intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_VOICE_STATES"], partials: ["USER", "CHANNEL", "GUILD_MEMBER", "MESSAGE", "REACTION"]});
 client.start();
